24988728b2d7e68bc11b63225fb9e109f599454c
Dylan <dylcg10@gmail.com>
2021-04-03 12:35:56 +1100

    hope this works

--- src/App.js
+++ src/App.js
@@ -1,35 +1,74 @@
-import logo from './logo.svg';
+import React, { Fragment, useEffect } from 'react';
+import Conversations from '@twilio/conversations';
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+
+import LoginForm from './LoginForm/index.js';
+import ChatRooms from './ChatRooms/index.js';
+import ChatLog from './ChatLog/index.js';
+import ChatInput from './ChatInput/index.js';
+import Sidebar from './Sidebar';
+import UserProfile from './UserProfile';
+
 import './App.css';
-import { useEffect, useState } from 'react';
+import Webinars from './Webinars/index.js';
+
+export const ChatContext = React.createContext();
 
 function App() {
-  const [currentTime, setCurrentTime] = useState(0);
+  const [user, setUser] = React.useState({api: null, username: null, chatrooms: [], classrooms: []});
+  const [selectedChatroom, setSelectedChatroom] = React.useState(null);
 
-  useEffect(() => {
-    fetch("/time").then(res => res.json()).then(data => {
-      setCurrentTime(data.time);
-    })
-  }, []);
+  const chatData = {
+    user: user,
+    selectedChatroom: selectedChatroom,
+  
+  }
+  // useEffect(() => {
+  //   fetch("/time").then(res => res.json()).then(data => {
+  //     setCurrentTime(data.time);
+  //   })
+  // }, []);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>Twilio Conversations API Demo</h1>
+      {/* <ChatContext.Provider value={chatData}>
+        <LoginForm />
+        {chatData.user.username !== null &&
+          <div id="chat">
+          <Sidebar/>
+          <ChatLog />
+          <ChatInput />
 
-        <p>The current time is: {currentTime}</p>
-      </header>
+          </div>
+        }
+      </ChatContext.Provider> */}
+      <ChatContext.Provider value={chatData}>
+        <HashRouter>
+          <Route path = "/" component = {LoginForm} />
+          {chatData.user.username !== null &&
+            <div>
+              <div id="chat">
+              <Route path="/main" render={() =>
+                    <Fragment>
+                      <Sidebar/>
+                      <ChatLog />
+                      <ChatInput />
+                    </Fragment>
+            }/>
+            </div>
+            <Route path="/profile" component={UserProfile} />
+            <Route path = "/webinars" component = {Webinars} />
+            </div>
+          }
+        </HashRouter>
+      </ChatContext.Provider>
     </div>
+
   );
 }
 
